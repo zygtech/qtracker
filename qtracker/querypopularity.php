99d9deb78a430be30403930b78501d95272fc19e
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-06 20:41:43 +0100

    Update querypopularity.php

--- qtracker/querypopularity.php
+++ qtracker/querypopularity.php
@@ -1,5 +1,5 @@
 <?php
-	$stream = stream_context_create(Array('http' => Array('method'  => 'GET','timeout' => 20,'header'  => 'User-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36')));
+	$stream = stream_context_create(Array('http' => Array('method'  => 'GET','header'  => 'User-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36')));
 	$file=fopen('https://www.google.com/search?q=' . urlencode($_GET['q']),'r',false,$stream);
 	$id='cwiwob VKx1id';
 	while (!feof($file)) { 
