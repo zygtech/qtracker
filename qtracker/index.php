fd08ac0417c62fbf1789f35e54e625a2922aa608
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-07 14:56:33 +0100

    Update index.php

--- qtracker/index.php
+++ qtracker/index.php
@@ -4,7 +4,7 @@
 -->
 <html>
 <head>
-	<title>Popularity Tracker</title>
+	<title>Query Popularity Tracker</title>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw==" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css" integrity="sha512-/zs32ZEJh+/EO2N1b0PEdoA10JkdC3zJ8L5FTiQu82LR9S/rOQNfQN7U59U9BC12swNeRAz3HSzIL2vpp4fv3w==" crossorigin="anonymous" />
 	<style>
